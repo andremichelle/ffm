26e5567d3973f128b0187ea81206bc48f8439557
André Michelle <andre.michelle@gmail.com>
2023-12-08 21:46:40 +0100

    clamps progress

--- src/components/Progress.tsx
+++ src/components/Progress.tsx
@@ -5,7 +5,7 @@ import React from "react"
 export const Progress = ({ value, className }: { value: unitValue, className?: string }) => {
     return (
         <div className={`progress ${className ?? ""}`}>
-            <div style={{ "--value": value } as React.CSSProperties}></div>
+            <div style={{ "--value": Math.min(1.0, Math.max(0.0, value)) } as React.CSSProperties}></div>
         </div>
     )
 }
