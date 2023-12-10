fb2e0fc69784e9ff1b5bd62543596ed8e0ff12d3
André Michelle <andre.michelle@gmail.com>
2023-12-10 18:44:27 +0100

    new icons

--- src/components/ConversionResult.tsx
+++ src/components/ConversionResult.tsx
@@ -35,16 +35,17 @@ export const ConversionResult = ({ fileNameWithExtension, state }: ConversationR
                                 <audio controls src={objectURL}></audio>
                                 <a href={objectURL} download={`${fileName}.wav`}
                                    onClick={event => event.stopPropagation()}>
-                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                                        <path
-                                            d="M13 12H16L12 16L8 12H11V8H13V12ZM15 4H5V20H19V8H15V4ZM3 2.9918C3 2.44405 3.44749 2 3.9985 2H16L20.9997 7L21 20.9925C21 21.5489 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918Z" />
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                              d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
                                     </svg>
                                 </a>
                             </div>
                             <div className="file-info hidden" ref={infoRef}>
                                 {state.value.meta_data.map((pair: KeyValuePair, index: int) =>
                                     <React.Fragment key={pair.key + index}>
-                                        <span>{pair.key}</span>
+                                    <span>{pair.key}</span>
                                         <span>{pair.value}</span>
                                     </React.Fragment>
                                 )}
