67b2deedc2cf38c67576a2100c7166a0d7cf94b8
André Michelle <andre.michelle@gmail.com>
2023-12-10 07:42:36 +0100

    removes details

--- src/components/ConversionResult.tsx
+++ src/components/ConversionResult.tsx
@@ -1,5 +1,5 @@
 import "./ConversionResult.sass"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { int, KeyValuePair } from "../common/lang"
 import { FileConversionResult } from "../ffmepg.ts"
 
@@ -9,32 +9,50 @@ type ConversationResultProps = {
 }
 
 export const ConversionResult = ({ fileNameWithExtension, state }: ConversationResultProps) => {
+    const [showMetaData, setShowMetaData] = useState(false)
+    const [objectURL, setObjectURL] = useState<string>("")
+
+    console.log("showMetaData", showMetaData)
+
+    // This feels so ludicrous to do.
+    useEffect(() => {
+        if (state.status === "fulfilled") {
+            const url = URL.createObjectURL(new Blob([state.value.file_data], { type: "audio/wav" }))
+            setObjectURL(url)
+            return () => URL.revokeObjectURL(url)
+        }
+    }, [])
+
     return (
         <div className="conversion-result">
             {(() => {
                 if (state.status === "fulfilled") {
-                    const objectURL = URL.createObjectURL(new Blob([state.value.file_data], { type: "audio/wav" }))
                     const fileName = fileNameWithExtension.substring(0, fileNameWithExtension.lastIndexOf("."))
                     return (
-                        <details>
-                            <summary>
-                                <div className="name">{fileNameWithExtension}</div>
+                        <>
+                            <div className="file-header">
+                                <div className="name"
+                                     onClick={() => setShowMetaData(!showMetaData)}>{fileNameWithExtension}</div>
                                 <audio controls src={objectURL}></audio>
-                                <a href={objectURL} download={`${fileName}.wav`}>
+                                <a href={objectURL} download={`${fileName}.wav`}
+                                   onClick={event => event.stopPropagation()}>
                                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                                         <path
                                             d="M13 12H16L12 16L8 12H11V8H13V12ZM15 4H5V20H19V8H15V4ZM3 2.9918C3 2.44405 3.44749 2 3.9985 2H16L20.9997 7L21 20.9925C21 21.5489 20.5551 22 20.0066 22H3.9934C3.44476 22 3 21.5447 3 21.0082V2.9918Z" />
                                     </svg>
                                 </a>
-                            </summary>
-                            <div className="file-info">{
-                                state.value.meta_data.map((pair: KeyValuePair, index: int) =>
-                                    <React.Fragment key={pair.key + index}>
-                                        <span>{pair.key}</span>
-                                        <span>{pair.value}</span>
-                                    </React.Fragment>)}
                             </div>
-                        </details>)
+                            {showMetaData && (
+                                <div className="file-info">
+                                    {state.value.meta_data.map((pair: KeyValuePair, index: int) =>
+                                        <React.Fragment key={pair.key + index}>
+                                            <span>{pair.key}</span>
+                                            <span>{pair.value}</span>
+                                        </React.Fragment>
+                                    )}
+                                </div>
+                            )}
+                        </>)
                 } else {
                     return (
                         <div className="failure">
