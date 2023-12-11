8758126291a9ed52df6bb8b27227bec321638272
André Michelle <andre.michelle@gmail.com>
2023-12-11 08:34:21 +0100

    highlight playing

--- src/components/ConversionResult.tsx
+++ src/components/ConversionResult.tsx
@@ -9,6 +9,7 @@ type ConversationResultProps = {
 }
 
 export const ConversionResult = ({ fileNameWithExtension, state }: ConversationResultProps) => {
+    const nameRef = useRef<HTMLDivElement>(null)
     const infoRef = useRef<HTMLDivElement>(null)
     const [objectURL, setObjectURL] = useState("")
     useEffect(() => {
@@ -29,15 +30,19 @@ export const ConversionResult = ({ fileNameWithExtension, state }: ConversationR
                         <>
                             <div className="file-header">
                                 <div className="name"
+                                     ref={nameRef}
                                      onClick={() => asDefined(infoRef.current).classList.toggle("hidden")}>
                                     {fileNameWithExtension}
                                 </div>
-                                <audio controls src={objectURL}></audio>
+                                <audio controls src={objectURL}
+                                       onPlay={() => nameRef.current?.classList.add("playing")}
+                                       onPause={() => nameRef.current?.classList.remove("playing")}
+                                ></audio>
                                 <a href={objectURL} download={`${fileName}.wav`}
                                    onClick={event => event.stopPropagation()}>
                                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                                         stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                         strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
+                                        <path stroke-cap="round" stroke-join="round"
                                               d="M9 8.25H7.5a2.25 2.25 0 00-2.25 2.25v9a2.25 2.25 0 002.25 2.25h9a2.25 2.25 0 002.25-2.25v-9a2.25 2.25 0 00-2.25-2.25H15M9 12l3 3m0 0l3-3m-3 3V2.25" />
                                     </svg>
                                 </a>
@@ -45,7 +50,7 @@ export const ConversionResult = ({ fileNameWithExtension, state }: ConversationR
                             <div className="file-info hidden" ref={infoRef}>
                                 {state.value.meta_data.map((pair: KeyValuePair, index: int) =>
                                     <React.Fragment key={pair.key + index}>
-                                    <span>{pair.key}</span>
+                                        <span>{pair.key}</span>
                                         <span>{pair.value}</span>
                                     </React.Fragment>
                                 )}
